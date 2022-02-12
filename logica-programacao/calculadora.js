6900a52f64d5a12d9521588aedf179472b32abb4
Thales Vanzo Vieira <thales.v.vieira@gmail.com>
2022-02-11 22:47:26 -0300

    calculadora incompleta

---
+++ logica-programacao/calculadora.js
@@ -0,0 +1,15 @@
+console.log("=== CALCULADORA ===");
+
+function calcular() {
+    console.log("CLICK!!!");
+
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    console.log(num1, num2);
+
+    const resultado = document.getElementById("resultado").write;
+
+
+    console.log(resultado);
+}
--- logica-programacao/funcoes.js
+++ logica-programacao/funcoes.js
@@ -1,2 +1,22 @@
 console.log("=== FUNÇÕES ===");
 
+// Função Nomeada
+function soma(n1=0, n2=0) {  // defaut parameter
+    return n1 + n2;
+}
+
+// NaN - Not a Number
+console.log(soma); // Ref Func
+console.log(soma()); // 0
+console.log(soma(5)); // 5
+console.log(soma(5, 10)); // 15
+
+// Declaralção de Função Anônima (Não nomeada)
+const sum = function (a=0, b=0) {
+    return a + b;
+}
+
+console.log( sum ); // ref função
+console.log( sum() ); // 0
+console.log( sum(5) ); // 5
+console.log( sum(5, 10) ); // 15
