50e6c4a14ddc422d8bfe0b75bde69d7a40b4a802
Thales Vanzo Vieira <thales.v.vieira@gmail.com>
2022-02-14 22:12:39 -0300

    desafio aula 13

--- logica-programacao/comparacao.js
+++ logica-programacao/comparacao.js
@@ -1,6 +1,9 @@
-function comparar(){
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
-    let comparacao = num1 > num2;
+function comparacao(){
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const comparacao = parseFloat(num1.value) > parseFloat(num2.value);
     document.getElementById("result").value = comparacao;
 }
+
+
+// usar a função switch para ajustar para qualquer tipo de comparação 
