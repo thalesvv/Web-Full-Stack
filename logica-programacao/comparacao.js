669402ebbf8454e51619ae97137135603f75203b
Thales Vanzo Vieira <thales.v.vieira@gmail.com>
2022-02-17 16:54:50 -0300

    desafio aula 13

--- logica-programacao/comparacao.js
+++ logica-programacao/comparacao.js
@@ -1,9 +1,47 @@
+/*
 function comparacao(){
     const num1 = document.getElementById("num1");
     const num2 = document.getElementById("num2");
     const comparacao = parseFloat(num1.value) > parseFloat(num2.value);
     document.getElementById("result").value = comparacao;
 }
+*/
 
 
-// usar a função switch para ajustar para qualquer tipo de comparação 
+function comparacao(){
+
+    // AQUISIÇÃO
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const operador = document.getElementById("operador");
+    let comparacao = null;
+   
+    // PROCESSAMENTO
+    switch (operador.value) {
+        case ">":
+            comparacao = parseFloat(num1.value) > parseFloat(num2.value);
+            break;
+        case "<":
+            comparacao = parseFloat(num1.value) < parseFloat(num2.value);
+            break;
+        case "!=":
+            comparacao = parseFloat(num1.value) != parseFloat(num2.value);
+            break;
+        case "==":
+            comparacao = parseFloat(num1.value) == parseFloat(num2.value);
+            break;
+        case ">=":
+            comparacao = parseFloat(num1.value) >= parseFloat(num2.value);
+            break;
+        case "<=":
+            comparacao = parseFloat(num1.value) <= parseFloat(num2.value);
+            break;
+    
+        default:
+            comparacao = "errou";
+            break;
+    }
+
+    // SAÍDA
+    document.getElementById("result").value = comparacao;
+}
